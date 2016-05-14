3a29808058cab6867a5142092a652a946e14ac5f
eloipuertas <eloipuertas@gmail.com>
2016-05-14 02:24:08 +0200

    comutador

--- python/testled.py
+++ python/testled.py
@@ -1,5 +1,25 @@
 import RPi.GPIO as GPIO
+import sys 
+import time
+
+value = False 
+
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.OUT)
-GPIO.output(17,1);
+GPIO.setup(27, GPIO.IN)
+GPIO.output(17,False)
+lastchange = value
+ledvalue = False 
+while (True):
+    try :
+	value = GPIO.input(27)
+	if value and not lastchange:
+            ledvalue = not ledvalue 
+	    GPIO.output(17, ledvalue)
+    	lastchange = value
+        time.sleep(0.02)
+    except (KeyboardInterrupt ):
+     	GPIO.cleanup()
+      	break
 
