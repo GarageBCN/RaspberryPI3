b672787235d0524a5c95181630f77a5a4fd3a385
Eloi Puertas <eloipuertas@gmail.com>
2016-05-15 02:49:28 +0200

    Add Handler for signal term

--- python/testled.py
+++ python/testled.py
@@ -1,8 +1,18 @@
 import RPi.GPIO as GPIO
-import sys 
+import sys
 import time
+import signal
 
-value = False 
+
+def handler(signum, frame):
+
+    GPIO.cleanup()
+
+# Set the signal handler and a 5-second alarm
+signal.signal(signal.SIGTERM, handler)
+
+
+value = False
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
@@ -10,16 +20,14 @@ GPIO.setup(17, GPIO.OUT)
 GPIO.setup(27, GPIO.IN)
 GPIO.output(17,False)
 lastchange = value
-ledvalue = False 
 while (True):
-    try :
+    #try :
 	value = GPIO.input(27)
 	if value and not lastchange:
-            ledvalue = not ledvalue 
+            ledvalue = not ledvalue
 	    GPIO.output(17, ledvalue)
     	lastchange = value
         time.sleep(0.02)
-    except (KeyboardInterrupt ):
-     	GPIO.cleanup()
-      	break
-
+    #except (KeyboardInterrupt ):
+    # 	GPIO.cleanup()
+    #  	break
